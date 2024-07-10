b91908065d76c78565e5400b0c1087df6fdddd8f
Pedro Cajas <pc@fibbo.app>
2024-07-10 00:47:25 -0600

    Trabajando con variables

--- app/src/main/java/com/pboox/androidmaster/Variables.kt
+++ app/src/main/java/com/pboox/androidmaster/Variables.kt
@@ -13,7 +13,7 @@ fun main(){
 */
 
 //    Int -2 a 2,147,483,647
-    val age:Int = 10
+    val age:Int = 45
 
     var age2:Int = 15
     println(age2)
@@ -26,6 +26,25 @@ fun main(){
 //    Double
     val doubleExample:Double = 123.41231423123
 
+    println("Sumar:")
+    println(age + age2)
+
+    println("Restar")
+    println(age - age2)
+
+    println("Multiplicar")
+    println(age * age2)
+
+    println("División")
+    println(age / age2)
+
+    println("Módulo")
+    println(age % age2)
+
+    var exampleSuma = age + floatExample
+    println("Suma de variable con diferente tipo")
+    println(exampleSuma)
+
 /**
  *  Variables alfanumericas
  */
@@ -36,11 +55,25 @@ fun main(){
 
 //    String
     val stringExample:String = "Pedro Cajas"
+    val stringExample2:String = "2"
+    val stringExample3:String = "45"
 
 //    Boolean
     val booleanExample:Boolean = true
     val booleanExample2:Boolean = false
     val booleanExample3 = true
 
+//    Concatenar
 
+    println(stringExample2 + stringExample3)
+
+//    referenciar variables dentro de string
+    var stringConcatenada: String = "Hola"
+    println(stringConcatenada)
+    stringConcatenada = "Hola me llamo $stringExample"
+    println(stringConcatenada)
+
+//    convertir variables
+    val exampleStringInt: String = age.toString()
+    println(exampleStringInt)
 }
