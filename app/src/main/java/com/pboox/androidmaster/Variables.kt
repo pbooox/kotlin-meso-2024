5babec83617b6128a8615f233a9cdf2e3dfb33ff
Pedro Cajas <pc@fibbo.app>
2024-07-10 00:17:19 -0600

    primer commit y variables

---
+++ app/src/main/java/com/pboox/androidmaster/Variables.kt
@@ -0,0 +1,46 @@
+package com.pboox.androidmaster
+
+// VARIABLES
+
+/**
+ * Variables
+ */
+
+fun main(){
+
+/**
+*   Variables numericas
+*/
+
+//    Int -2 a 2,147,483,647
+    val age:Int = 10
+
+    var age2:Int = 15
+    println(age2)
+    age2 = 16
+    println(age)
+//    Long
+    val example:Long = 30
+//    Float
+    val floatExample:Float = 30.5f
+//    Double
+    val doubleExample:Double = 123.41231423123
+
+/**
+ *  Variables alfanumericas
+ */
+//    Char
+    val charExample1:Char = 'e'
+    val charExample2:Char = '2'
+    val charExample3:Char = '@'
+
+//    String
+    val stringExample:String = "Pedro Cajas"
+
+//    Boolean
+    val booleanExample:Boolean = true
+    val booleanExample2:Boolean = false
+    val booleanExample3 = true
+
+
+}
