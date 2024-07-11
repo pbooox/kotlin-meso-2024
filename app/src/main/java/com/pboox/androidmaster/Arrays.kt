5d2a0850e204e52f65f9c78897a1a583057ad7b3
Pedro Cajas <pc@fibbo.app>
2024-07-10 23:50:32 -0600

    arrays y listas

---
+++ app/src/main/java/com/pboox/androidmaster/Arrays.kt
@@ -0,0 +1,38 @@
+package com.pboox.androidmaster
+
+fun main(){
+//    Indice 0-6
+//    Tamaño 7
+    val weekDays = arrayOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
+
+//    println(weekDays[0])
+//    println(weekDays.size)
+
+    //Tamaños
+    if (weekDays.size >= 7) {
+//        println(weekDays[6])
+    } else {
+//        println("No hay mas valores en el array")
+    }
+
+    //Modificar valores
+//    weekDays[0] = "Feliz lunes"
+//    println(weekDays[0])
+
+
+    //Bucles para Arrays
+    for(position in weekDays.indices){
+//       println(weekDays[position])
+    }
+
+    for((position, value) in weekDays.withIndex()){
+//        println("La posición $position, contiene $value")
+    }
+
+    for (weekday in weekDays){
+//        println("Ahora es $weekday")
+    }
+
+//    weekDays.forEach { println(it) }
+    weekDays.forEach { weekDay -> println(weekDay) }
+}
---
+++ app/src/main/java/com/pboox/androidmaster/Nulabilidad.kt
@@ -0,0 +1,7 @@
+package com.pboox.androidmaster
+
+fun main () {
+    var name:String? = null
+
+    println(name?.get(0) ?: "es null")
+}
