1740d0013a72710972cd1f03bca4fc1401c64aa9
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-07-16 23:44:57 -0600

    linearlayout y constraintlayout

---
+++ app/src/main/java/com/pboox/androidmaster/exampleLayout/ConstraintLayoutActivity.kt
@@ -0,0 +1,21 @@
+package com.pboox.androidmaster.exampleLayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pboox.androidmaster.R
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_constraint_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintLayoutMain)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
