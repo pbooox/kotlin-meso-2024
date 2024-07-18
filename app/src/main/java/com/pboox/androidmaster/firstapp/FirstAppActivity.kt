123488d1c28e43913c67a3ad14a114a0d5f77ad9
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-07-17 22:37:22 -0600

    Firstapp intent

--- app/src/main/java/com/pboox/androidmaster/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/pboox/androidmaster/firstapp/FirstAppActivity.kt
@@ -1,21 +1,40 @@
 package com.pboox.androidmaster.firstapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pboox.androidmaster.R
+import android.util.Log
+import androidx.appcompat.widget.AppCompatEditText
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainFirstAppMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener{
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                Log.i("Pedro", "Se ha pulsado el boton ${name}")
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("name", name)
+                startActivity(intent)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/pboox/androidmaster/firstapp/ResultActivity.kt
+++ app/src/main/java/com/pboox/androidmaster/firstapp/ResultActivity.kt
@@ -1,23 +1,25 @@
-package com.pboox.androidmaster.exampleLayout
+package com.pboox.androidmaster.firstapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pboox.androidmaster.R
 
-class ConstraintLayoutActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_constraint_layout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintLayoutMain)) { v, insets ->
+        setContentView(R.layout.activity_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val name: String = intent.extras?.getString("name").orEmpty()
+        tvResult.text = "Hola $name"
     }
-
-
 }
